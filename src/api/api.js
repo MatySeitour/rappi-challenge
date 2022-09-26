244380b72d9f96c708be50928f6d73716dd5c4dc
Matias Seitour <matias.seitour01@gmail.com>
2022-09-26 12:55:33 -0300

    add styles and api

---
+++ src/api/api.js
@@ -0,0 +1,7 @@
+import _products from './products.json'
+import _categories from './categories.json'
+
+export default {
+    getCategories: () => _categories.categories,
+    getProducts: () => _categories.categories,
+}

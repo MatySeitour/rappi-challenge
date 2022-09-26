9d978a46ad3c9ef77d06547d18913f276002021a
Matias Seitour <matias.seitour01@gmail.com>
2022-09-26 12:56:40 -0300

    add browser router in my app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import { useState } from 'react'
 import { Nav } from './Nav'
+import { Categories } from './Categories'
+import { SubCategory } from './SubCategory'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Products } from './Products'
 
 function App() {
 
   return (
-    <Nav />
+    <BrowserRouter>
+
+      <Nav />
+
+      <Routes>
+        <Route exact path='/' element={<Products />}/>
+        <Route exact path='/categories/:id' element={<Categories />}/>
+        <Route exact path='/categories/:idCategory/:idSubCategory' element={<SubCategory />}/>
+      </Routes>
+
+    </BrowserRouter>
   )
 }
 
